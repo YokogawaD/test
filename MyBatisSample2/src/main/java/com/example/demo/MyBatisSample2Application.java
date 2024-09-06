bbac9961914f4ceb7b68a87d3edd6a9b794ae0ab
3031037 <3031037@30310-37>
2024-09-06 13:26:12 +0900

    test

---
+++ MyBatisSample2/src/main/java/com/example/demo/MyBatisSample2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisSample2Application.class, args);
+	}
+
+}
